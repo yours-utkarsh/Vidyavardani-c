e278031fc055ee5f3c5f670ac9fceae9904f7526
utkarshg9981 <utkarshg9981@gmail.com>
2026-01-16 01:28:05 +0530

    write private route & open route
    define some routes in app.jsx all open routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import "./App.css";
 import { Route , Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/common/Navbar.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import UpdatePassword from "./pages/UpdatePassword.jsx";
+import Error from "./pages/Error.jsx";
+import OpenRoute from "";
+
 
 function App() {
   return (
@@ -9,6 +16,64 @@ function App() {
     <Navbar/>
     <Routes>
    <Route path="/" element={<Home/>} />
+   
+   <Route
+   path="signup"
+   element={
+    <OpenRoute>
+     <Signup/>
+    </OpenRoute>
+   }
+   />
+
+   <Route
+   path="login"
+   element={
+    <OpenRoute>
+     <Login/>
+    </OpenRoute>
+   }
+   />
+
+   <Route
+   path="forgot-password"
+   element={
+    <OpenRoute>
+     <ForgotPassword/>
+    </OpenRoute>
+   }
+   />
+
+   <Route
+   path="forgot-password"
+   element={
+    <OpenRoute>
+     <ForgotPassword/>
+    </OpenRoute>
+   }
+   />
+
+   <Route
+   path="update-password/:id"
+   element={
+    <OpenRoute>
+     <UpdatePassword/>
+    </OpenRoute>
+   }
+   />
+
+   <Route
+   path="verify-email"
+   element={
+    <OpenRoute>
+     <ForgotPassword/>
+    </OpenRoute>
+   }
+   />
+
+
+   <Route path="*" element={<Error/>} />
+
     </Routes>
     
     
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const ForgotPassword = () => {
+
+  const [email , setEmail] = useState("")
+  const {loading} = useSelector((state) => state.auth);
+  const [emailSent , setEmailSent] = useState(false);
+
   return (
     <div>
-      
+      {
+        loading ? (
+        <div>
+
+          loading....
+        </div>
+        ):(
+          <div>
+            <h1>
+              {
+                !emailSent ?  "Reset Your Password" :  "check your email"
+              }
+            </h1>
+
+            <p>
+              {
+                  !emailSent 
+                  ? "Have no fear. We will email you instructions to reset your password. If you don't have your email access we can try account recovery" 
+                  : `We have sent the reset email to ${email}`
+              }
+            </p>
+
+            <form>
+              {
+                !emailSent && (
+                  <label>
+                    <p>Email Address</p>
+                    <input 
+                    type="email"
+                    required
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder='Enter your email address'
+                    />
+                  </label>
+                )
+              }
+              <button>
+                {
+                  !emailSent ? "Reset Password " : "Resend Email"
+                }
+              </button>
+            </form>
+
+            <div>
+              <Link to="/login" >
+              <p>Back To login</p>
+              </Link>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
