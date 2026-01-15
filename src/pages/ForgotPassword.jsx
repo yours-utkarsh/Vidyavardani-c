85112417d42eba73dcf2c84b2094e7bdd8c7e0f6
utkarshg9981 <utkarshg9981@gmail.com>
2026-01-16 01:51:30 +0530

    feat: add Spinner component and improve loading state handling in ForgotPassword page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,70 +1,54 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Spinner from "../components/common/Spinner";
 const ForgotPassword = () => {
-
-  const [email , setEmail] = useState("")
-  const {loading} = useSelector((state) => state.auth);
-  const [emailSent , setEmailSent] = useState(false);
+  const [email, setEmail] = useState("");
+  const { loading } = useSelector((state) => state.auth);
+  const [emailSent, setEmailSent] = useState(false);
 
   return (
     <div>
-      {
-        loading ? (
+      {loading ? (
         <div>
-
-          loading....
+          <Spinner />
         </div>
-        ):(
+      ) : (
+        <div>
+          <h1>{!emailSent ? "Reset Your Password" : "check your email"}</h1>
+
+          <p>
+            {!emailSent
+              ? "Have no fear. We will email you instructions to reset your password. If you don't have your email access we can try account recovery"
+              : `We have sent the reset email to ${email}`}
+          </p>
+
+          <form>
+            {!emailSent && (
+              <label>
+                <p>Email Address</p>
+                <input
+                  type="email"
+                  required
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email address"
+                />
+              </label>
+            )}
+            <button>{!emailSent ? "Reset Password " : "Resend Email"}</button>
+          </form>
+
           <div>
-            <h1>
-              {
-                !emailSent ?  "Reset Your Password" :  "check your email"
-              }
-            </h1>
-
-            <p>
-              {
-                  !emailSent 
-                  ? "Have no fear. We will email you instructions to reset your password. If you don't have your email access we can try account recovery" 
-                  : `We have sent the reset email to ${email}`
-              }
-            </p>
-
-            <form>
-              {
-                !emailSent && (
-                  <label>
-                    <p>Email Address</p>
-                    <input 
-                    type="email"
-                    required
-                    name='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder='Enter your email address'
-                    />
-                  </label>
-                )
-              }
-              <button>
-                {
-                  !emailSent ? "Reset Password " : "Resend Email"
-                }
-              </button>
-            </form>
-
-            <div>
-              <Link to="/login" onClick={() => setEmailSent(false)}>
+            <Link to="/login" onClick={() => setEmailSent(false)}>
               <p>Back To login</p>
-              </Link>
-            </div>
+            </Link>
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
