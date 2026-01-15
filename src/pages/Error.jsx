b0ce9b73d4bcab5ef877089f50a141ee91778e39
utkarshg9981 <utkarshg9981@gmail.com>
2026-01-15 23:43:20 +0530

    set env on right folder and establise connection between showallcategories function and frontend

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div  className='flex justify-center items-center text-3xl text-red' >
+      404 - Page not found
+    </div>
+  )
+}
+
+export default Error
