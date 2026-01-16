eebaf3ebc78af1a11f0547e8a11563541abbec11
utkarshg9981 <utkarshg9981@gmail.com>
2026-01-17 01:13:01 +0530

    feat: creating update password page

--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Spinner from "../components/common/Spinner";
 
 const UpdatePassword = () => {
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { loading } = useSelector((state) => state.auth);
+
+  const handleOnChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div>
-      
+      {loading ? (
+        <Spinner></Spinner>
+      ) : (
+        <div>
+          <h1>Choose New Password</h1>
+          <p>Almost done. Enter your new password & you are all set.</p>
+          <form onSubmit={handleOnSubmit} >
+            <label htmlFor="">
+              <p>New Password</p>
+              <input
+                required
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleOnChange}
+              />
+            </label>
+            <label htmlFor="">
+              <p>Confirm Password</p>
+              <input
+                required
+                type={showConfirmPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleOnChange}
+              />
+            </label>
+          </form>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UpdatePassword
+export default UpdatePassword;
