c8062734017ad6621ea20d7b51f2caed253836ab
utkarshg9981 <utkarshg9981@gmail.com>
2026-01-18 17:35:24 +0530

    style: added style by creating and definig variables on app.css

--- src/components/core/Auth/LoginForm.jsx
+++ src/components/core/Auth/LoginForm.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+    const dispath = useDispatch()
+    const navigate = useNavigate()
+
+    const [formData , setFormData] = useState({
+        email : "",
+        password : "",
+    })
+
+    const [showPassword , setShowPassword ] = useState(false)
+
+    const {email , password} = formData
+
+    const handleOnChange = (e) =>{
+
+    }
+
+   const handleOnSubmit = (e) => {
+    e.preventDefault();
+    dispath(login(email , password , navigate))
+   }
+
   return (
-    <div>
+    <form
+    onSubmit={handleOnSubmit}
+    >
+
       
-    </div>
+      {/* first field  */}
+      <label htmlFor="">
+        <input 
+        type="email"
+        required
+        name='email'
+        value={email}
+        onChange={handleOnChange}
+        placeholder='Enter your email'
+        className='form'
+        />
+        
+      </label>
+
+      {/* second field  */}
+      <label htmlFor=""></label>
+
+      {/* button  */}
+      <button
+      type='submit'
+      >
+        Log In
+      </button>
+    </form>
   )
 }
 
