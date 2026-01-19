f9740fb24ac5a19134028f793f52c3b578d98253
utkarshg9981 <utkarshg9981@gmail.com>
2026-01-19 22:05:46 +0530

    feat: implement Sidebar component and refactor Dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Spinner from '../components/common/Spinner'
+import React from "react";
+import { useSelector } from "react-redux";
+import Spinner from "../components/common/Spinner";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    const {loading: authLoading} = useSelector((state) => state.auth)
-    const {loading: profileLoading} = useSelector((state) => state.profile)
-    
-    if(authLoading || profileLoading)
-    {
-        return (
-            <Spinner/>
-        )
-    }
+  const { loading: authLoading } = useSelector((state) => state.auth);
+  const { loading: profileLoading } = useSelector((state) => state.profile);
+
+  if (authLoading || profileLoading) {
+    return <Spinner />;
+  }
 
   return (
     <div>
-
-        <Sidebar/>
-      
+      <Sidebar />
+      <div>
+        <div>
+          <Outlet />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
