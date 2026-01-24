9bc5cf73b70172897dd44268a9f2c6303d7631e6
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-24 22:57:15 +0530

    feat: add content rendering for LearningGrid component

--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -21,7 +21,13 @@ const LearningGrid = () => {
             {card.order < 0 ? (
              
             ) : (
-              
+              <div className="p-8 flex flex-col gap-8">
+                <h1 className="text-richblack-5 text-lg">{card.heading}</h1>
+
+                <p className="text-richblack-300 font-medium">
+                  {card.description}
+                </p>
+              </div>
             )}
           </div>
         );
