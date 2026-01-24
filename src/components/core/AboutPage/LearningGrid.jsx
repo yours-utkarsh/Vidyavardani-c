fa470a4c4d8851706562ffd45b88110cdbeb0e2b
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-24 22:58:05 +0530

    feat: add detailed content structure to LearningGrid component

--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -2,6 +2,47 @@ import React from "react";
 import HighlightText from "../HomePage/HighlightText";
 import CTAButton from "../HomePage/Button";
 
+const LearningGridArray = [
+  {
+    order: -1,
+    heading: "World-Class Learning for",
+    highliteText: "Anyone, Anywhere",
+    description:
+      "Learnify partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
+    BtnText: "Learn More",
+    BtnLink: "/",
+  },
+  {
+    order: 1,
+    heading: "Curriculum Based on Industry Needs",
+    description:
+      "Save time and money! The Belajar curriculum is made to be easier to understand and in line with industry needs.",
+  },
+  {
+    order: 2,
+    heading: "Our Learning Methods",
+    description:
+      "Learnify partners with more than 275+ leading universities and companies to bring",
+  },
+  {
+    order: 3,
+    heading: "Certification",
+    description:
+      "Learnify partners with more than 275+ leading universities and companies to bring",
+  },
+  {
+    order: 4,
+    heading: `Rating "Auto-grading"`,
+    description:
+      "Learnify partners with more than 275+ leading universities and companies to bring",
+  },
+  {
+    order: 5,
+    heading: "Ready to Work",
+    description:
+      "Learnify partners with more than 275+ leading universities and companies to bring",
+  },
+];
 
 const LearningGrid = () => {
   return (
