128f2e1e413428ac85a708940408354ae9315aa9
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-24 22:56:59 +0530

    feat: implement LearningGrid component for About page

---
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import HighlightText from "../HomePage/HighlightText";
+import CTAButton from "../HomePage/Button";
+
+
+const LearningGrid = () => {
+  return (
+    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+      {LearningGridArray.map((card, i) => {
+        return (
+          <div
+            key={i}
+            className={`${i === 0 && "xl:col-span-2 xl:h-[294px]"}  ${
+              card.order % 2 === 1
+                ? "bg-richblack-700 h-[294px]"
+                : card.order % 2 === 0
+                ? "bg-richblack-800 h-[294px]"
+                : "bg-transparent"
+            } ${card.order === 3 && "xl:col-start-2"}  `}
+          >
+            {card.order < 0 ? (
+             
+            ) : (
+              
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default LearningGrid;
