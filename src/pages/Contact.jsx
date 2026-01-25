7037f3ec1dfc12da853d54de5849ecb8c4f63c05
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-25 19:40:26 +0530

    style: format Contact page code for improved readability

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
-import Footer from '../components/common/Footer'
-import ContactPageForm from '../components/core/ContactPage.jsx/ContactPageForm'
-import ContactDetails from '../components/core/ContactPage.jsx/ContactDetails'
+import React from "react";
+import Footer from "../components/common/Footer";
+import ContactPageForm from "../components/core/ContactPage.jsx/ContactPageForm";
+import ContactDetails from "../components/core/ContactPage.jsx/ContactDetails";
 
-const Contact  = () => {
+const Contact = () => {
   return (
     <div>
-     {/* upper page  */}
+      {/* upper page  */}
       <div>
-    <div>
-      <ContactDetails/>
-     </div>
+        <div>
+          <ContactDetails />
+        </div>
 
-     <div>
-     <ContactPageForm />
-
-     </div>
+        <div>
+          <ContactPageForm />
+        </div>
       </div>
-      
 
-   
-
-
-     {/* footer  */}
-     <Footer />
+      {/* footer  */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
