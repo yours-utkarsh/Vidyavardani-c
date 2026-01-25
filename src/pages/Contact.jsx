f9ae263b3d8ba4eaaa1e57c32742c80cfff1c44e
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-25 19:40:38 +0530

    style: improve layout structure of Contact page for better readability

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,9 @@ const Contact = () => {
   return (
     <div>
       {/* upper page  */}
-      <div>
-        <div>
+      <div className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row mb-8">
+        {/* Contact Details */}
+        <div className="lg:w-[40%]">
           <ContactDetails />
         </div>
 
