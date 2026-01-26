a262f70c2a8bdaa8d99c01378b67d6734d1482b5
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-26 17:10:28 +0530

    feat: enhance ContactPageForm with structured layout and improved styling

--- src/components/core/ContactPage.jsx/ContactPageForm.jsx
+++ src/components/core/ContactPage.jsx/ContactPageForm.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import ContactForm from "./ContactForm";
+
 
 const ContactPageForm = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="border border-richblack-600 text-richblack-300 rounded-xl p-7 lg:p-14 flex gap-3 flex-col">
+      <h1 className="text-4xl leading-10 font-semibold text-richblack-5">
+        Got a Idea? We&apos;ve got the skills. Let&apos;s team up
+      </h1>
+      <p className="">
+        Tell us more about yourself and what you&apos;re got in mind.
+      </p>
 
-export default ContactPageForm
+      <div className="mt-7">
+        <ContactForm/>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPageForm;
