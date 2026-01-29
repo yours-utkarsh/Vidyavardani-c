9a56f8849134fdd1c3402e41e776986d8c7725f7
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-01-29 22:40:10 +0530

    feat: enhance PrivateRoute component with loading state handling and improved token validation

--- src/components/core/Auth/PrivateRoute.jsx
+++ src/components/core/Auth/PrivateRoute.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
 
-const PrivateRoute = ({children}) => {
-    const {token} = useSelector((state) => state.auth)
+const PrivateRoute = ({ children }) => {
+  const { token } = useSelector((state) => state.auth);
+  const loading = useSelector((state) => state.auth.loading);
 
- if(token !== null){
-    return children
- }
- else{
-    return <Navigate to="/login" />
- }
-}
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-export default PrivateRoute
+  return token ? children || <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
