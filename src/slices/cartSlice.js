c55f04826862117e4c0eadf18a315e3a85ab7941
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-03 11:16:16 +0530

    feat: enhance cart functionality with toast notifications and localStorage updates

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 // import { toast } from "react-hot-toast";
 
 const initialState = {
+    cart: localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")):[],
+    total: localStorage.getItem("total") ? JSON.parse(localStorage.getItem("total")) : 0,
     totalItems : localStorage.getItem("totalItems") ? JSON.parse(localStorage.getItem("totalItems")) : 0,
 }
 
@@ -10,15 +13,52 @@ const cartSlice = createSlice({
     name:"cart",
     initialState:initialState,
     reducers :{
-        setTotalItems(state,value){
-            state.totalItems = value.payload;
-        }
-
         // add to cart 
+        addToCart : (state , action) =>{
+            const course = action.payload
+            const index = state.cart.findIndex((item) => item._id === course._id )
+
+            if(index >= 0){
+                toast.error("Cart is already added")
+                return
+            }
+
+            state.cart.push(course);
+            state.totalItems++
+            state.total += course.price
+            localStorage.setItem("cart" , JSON.stringify(state.cart))
+            localStorage.setItem("total" , JSON.stringify(state.total))
+            localStorage.setItem("cart" , JSON.stringify(state.totalItems))
+            toast.success("Course added to cart")
+        },
         // remove from cart
+
+        removeFromCart : (state , action) =>{
+
+            const courseId = action.payload
+            const index = state.cart.findIndex((item) => item._id === courseId)
+
+            if(index >= 0){
+                state.totalItems--;
+                state.total -= state.cart[index].price
+                state.cart.splice(index , 1);
+                localStorage.setItem("cart" , JSON.stringify(state.cart))
+                localStorage.setItem("total" , JSON.stringify(state.total))
+                localStorage.setItem("totalItems" , JSON.stringify(state.totalItems))
+                toast.success("Course removed from cart")
+            }
+        },
         // reset cart
+        resetCart : (state) =>{
+            state.cart = []
+            state.total = 0
+            state.totalItems = 0
+            localStorage.removeItem("cart")
+            localStorage.removeItem("total")
+            localStorage.removeItem("totalItems")
+        }
     }
 })
 
-export const {setTotalItems} = cartSlice.actions;
+export const {addToCart , removeFromCart , resetCart} = cartSlice.actions;
 export default cartSlice.reducer;
