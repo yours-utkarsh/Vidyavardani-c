11206aaa7f80db13e5f495d1166984ad2bd754d6
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-05 22:03:01 +0530

    feat: enhance updateProfile function to handle errors and improve user feedback

--- src/services/operations/settingsAPI.js
+++ src/services/operations/settingsAPI.js
@@ -1,6 +1,5 @@
 import toast from "react-hot-toast";
 import { apiConnector } from "../apiconnector";
-import { setToken } from "../../slices/authSlice";
 import { setUser, setLoading } from "../../slices/profileSlice";
 import { settingsEndpoints } from "../apis";
 
@@ -43,6 +42,33 @@ export function updateDisplayPicture(token, formData) {
     toast.dismiss(toastId);
   };
 }
+
+export function updateProfile(token, formData) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...")
+    try {
+      const response = await apiConnector("PUT", UPDATE_PROFILE_API, formData, {
+        Authorization: `Bearer ${token}`,
+      })
+
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
+      const userImage = response.data.updatedUserDetails.image
+        ? response.data.updatedUserDetails.image
+        : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.updatedUserDetails.firstName} ${response.data.updatedUserDetails.lastName}`
+      dispatch(
+        setUser({ ...response.data.updatedUserDetails, image: userImage })
+      )
+      toast.success("Profile Updated Successfully")
+    } catch (error) {
+      toast.error("Could Not Update Profile")
+    }
+    toast.dismiss(toastId)
+  }
+}
+
+
 
 export function deleteProfile(token, navigate) {
   return async (dispatch) => {
