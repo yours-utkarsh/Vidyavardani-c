f6bd43d65001470f4a13beb7e7a1f6b49a08e265
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-08 13:10:58 +0530

    feat: create Upload component for video file handling in course creation

---
+++ src/components/core/Dashboard/AddCourse/Upload.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect, useRef, useState } from "react"
+import { useDropzone } from "react-dropzone"
+import { FiUploadCloud } from "react-icons/fi"
+
+import "video-react/dist/video-react.css"
+import { Player } from "video-react"
+
+const Upload = ({
+  name,
+  label,
+  register,
+  setValue,
+  errors,
+  video = false,
+  viewData = null,
+  editData = null,
+}) => {
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Upload
