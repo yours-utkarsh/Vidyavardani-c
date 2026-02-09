830de890815791cfc98253e6faffefeac98c4050
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-09 07:19:56 +0530

    feat: enhance CourseInformationForm to support course editing and category fetching

--- src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {useForm} from "react-hook-form" 
 import Upload from '../Upload'
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const CourseInformationForm = () => {
+const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState :{errors}
+} = useForm();
+
+const dispatch = useDispatch()
+const {token } = useSelector((state) => state.auth)
+const {course , editCourse} = useSelector((state) => state.course)
+
+const [loading , setLoading] = useState(false)
+const [courseCategories , setCourseCategories] = useState([])
+
+useEffect(() => {
+    
+    const getCategories = async () => {
+        setLoading(true)
+
+        const categories = await fetchCourseCategories()
+        if(categories.length >0){
+            setCourseCategories(categories)
+        }
+        setLoading(false)
+    }
+
+     if(editCourse){
+        setValue("courseTitle", course.courseName)
+      setValue("courseShortDesc", course.courseDescription)
+      setValue("coursePrice", course.price)
+      setValue("courseTags", course.tag)
+      setValue("courseBenefits", course.whatYouWillLearn)
+      setValue("courseCategory", course.category)
+      setValue("courseRequirements", course.instructions)
+      setValue("courseImage", course.thumbnail)
+     }
+
+} , [])
+
 
 
   return (
