da67801805328d2eb2d0941db2e2d3354288f7ed
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-10 23:16:44 +0530

    imported  neccesary things

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { toast } from "react-hot-toast"
+import { IoAddCircleOutline } from "react-icons/io5"
+import { MdNavigateNext } from "react-icons/md"
+import { useDispatch, useSelector } from "react-redux"
+
+
+import {
+  createSection,
+  updateSection
+} from "../../../../../services/operations/courseDetailsAPI"
+import {
+  setCourse,
+  setEditCourse,
+  setStep,
+} from "../../../../../slices/courseSlice"
+import IconBtn from "../../../../common/IconBtn"
+import NestedView from "./NestedView"
 
 const CourseBuilderForm = () => {
+
+
+ 
   return (
     <div>
+      <p>Course Builder</p>
+
+      {/* course builder form  */}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+      </form>
+
+      {/* nested view  */}
+
+   
+      {/* Next prev button  */}
       
+      <div>
+
+    
+        
+       
+
+      </div>
+
     </div>
   )
 }
