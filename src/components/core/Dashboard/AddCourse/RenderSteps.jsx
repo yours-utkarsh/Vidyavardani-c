ec1921fce46721255383f36f99292e251a3bd456
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-11 21:47:25 +0530

    added publish

--- src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { FaCheck } from "react-icons/fa"
 import CourseInformationForm from "./CourseInformation/CourseInformationForm"
 import CourseBuilderForm from "./CourseBuilder/CourseBuilderForm"
-import PublishCourse from "./CourseBuilder/CourseBuilderForm"
+import PublishCourse from "./PublishCourse/Publish"
 
 const RenderSteps = () => {
   const { step } = useSelector((state) => state.course);
