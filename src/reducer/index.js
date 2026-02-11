4b8ee02a7145a5a553c81fb68a220eaec2e46aea
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-11 22:10:34 +0530

    add course and viewCourse reducers to rootReducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,12 +2,16 @@ import {combineReducers} from "@reduxjs/toolkit"
 import authReducer from "../slices/authSlice.js"
 import profileReducer from "../slices/profileSlice.js"
 import cartReducer from "../slices/cartSlice.js"
+import courseReducer from "../slices/courseSlice.js"
+import viewCourseReducer from "../slices/viewCourseSlice.js"
 
 
 const rootReducer = combineReducers({
         auth : authReducer,
         profile : profileReducer,
-        cart : cartReducer
+        cart : cartReducer,
+        course : courseReducer,
+        viewCourse : viewCourseReducer
 })
 
 export default rootReducer;
