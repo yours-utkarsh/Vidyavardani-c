21f218dc2b10cd0cb64428052f393275ffb7b424
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-11 10:00:03 +0530

    feat: create section creation form

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -109,9 +109,46 @@ const CourseBuilderForm = () => {
 
       {/* course builder form  */}
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-
+    
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="flex flex-col space-y-2">
+          <label className="text-sm text-richblack-5" htmlFor="sectionName">
+            Section Name <sup className="text-pink-200">*</sup>
+          </label>
+          <input
+            id="sectionName"
+            disabled={loading}
+            placeholder="Add a section to build your course"
+            {...register("sectionName", { required: true })}
+            className="form-style w-full"
+          />
+          {errors.sectionName && (
+            <span className="ml-2 text-xs tracking-wide text-pink-200">
+              Section name is required
+            </span>
+          )}
+        </div>
+        <div className="flex items-end gap-x-4">
+          <IconBtn
+            type="submit"
+            disabled={loading}
+            text={editSectionName ? "Edit Section Name" : "Create Section"}
+            outline={true}
+          >
+            <IoAddCircleOutline size={20} className="text-yellow-50" />
+          </IconBtn>
+          {editSectionName && (
+            <button
+              type="button"
+              onClick={cancelEdit}
+              className="text-sm text-richblack-300 underline"
+            >
+              Cancel Edit
+            </button>
+          )}
+        </div>
       </form>
+
 
       {/* nested view  */}
 
