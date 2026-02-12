7537481f879baed1012ea331d4b92e2836e1adb1
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-12 21:46:46 +0530

    update styling for Course Builder header in CourseBuilderForm component

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -100,9 +100,9 @@ const CourseBuilderForm = () => {
   };
 
   return (
-    <div>
-      <p>Course Builder</p>
+    <div className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
 
+      <p className="text-2xl font-semibold text-richblack-5">Course Builder</p>
       {/* course builder form  */}
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
