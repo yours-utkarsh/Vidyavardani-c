3abf1dedd8125ae8b3e9bc0512274beaf3bd45b5
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-12 20:53:29 +0530

    const variable establishemnt

--- src/services/formatDate.js
+++ src/services/formatDate.js
@@ -3,7 +3,11 @@ export const formatDate = (dateString) => {
   const date = new Date(dateString)
   const formattedDate = date.toLocaleDateString("en-US", options)
 
-
-  
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const period = hour >= 12 ? "PM" : "AM"
+  const formattedTime = `${hour % 12}:${minutes
+    .toString()
+    .padStart(2, "0")} ${period}`
 
 }
