33ecc027d4dd92ef3979266c8c2eb307fc3a59ad
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-18 00:12:30 +0530

    f

--- src/components/core/Auth/ProfileDropdown.jsx
+++ src/components/core/Auth/ProfileDropdown.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
+ProfileDropdown();
 
 const ProfileDropdown = () => {
   const navigate = useNavigate();
+ 
   return (
     <div className='text-white'>
       <button onClick={()=> navigate("/dashboard/my-profile")} >
-
+      const a =0;
+      a = 9
       Utkarsh gupta
       </button>
     </div>
   )
 }
+
+
 
 export default ProfileDropdown
