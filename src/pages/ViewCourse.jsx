a287216e5cc4de331cdbaed7e9edf01025083812
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-19 00:01:35 +0530

    add imports for viewCourseSlice actions in ViewCourse component

--- src/pages/ViewCourse.jsx
+++ src/pages/ViewCourse.jsx
@@ -5,6 +5,11 @@ import { Outlet, useParams } from "react-router-dom"
 import CourseReviewModal from "../Component/Core/ViewCourse/CourseReviewModal"
 import VideoDetailsSidebar from "../Component/Core/ViewCourse/VideoDetailsSidebar"
 import { getFullDetailsOfCourse } from "../Service/Operation/courseDetailsAPI"
-
+import {
+  setCompletedLectures,
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+} from "../Slice/viewCourseSlice"
 
 
