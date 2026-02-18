fa9f001fa4bfa54ca183edda90e3359618ee095c
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-18 23:57:11 +0530

    add description2 prop to Template component in Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
       <Template
       title="Welcome Back"
       description1="Build skills for today, tomorrow, and beyond."
-    
+      description2="Education to future-proof your career."
 
 
       />
