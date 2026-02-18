ce9cc09fe952613015321c1b77ff5000584cb07d
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-19 00:01:01 +0530

    add imports for CourseReviewModal and VideoDetailsSidebar in ViewCourse component

--- src/pages/ViewCourse.jsx
+++ src/pages/ViewCourse.jsx
@@ -1,3 +1,7 @@
+import { Outlet, useParams } from "react-router-dom"
+
+import CourseReviewModal from "../Component/Core/ViewCourse/CourseReviewModal"
+import VideoDetailsSidebar from "../Component/Core/ViewCourse/VideoDetailsSidebar"
 
 
 
