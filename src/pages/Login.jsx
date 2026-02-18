475ac29a4c2888952d1b861f20284e4b6833a44e
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-18 23:57:20 +0530

    add image prop to Template component in Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
       title="Welcome Back"
       description1="Build skills for today, tomorrow, and beyond."
       description2="Education to future-proof your career."
-
+      image={loginImg}
 
       />
     </div>
