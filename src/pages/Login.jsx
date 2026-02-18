7740c63d993343d7438df38be2664192c378c653
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-18 23:56:18 +0530

    add Login component structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import loginImg from '../asset/Image/login.webp'
 import Template from '../components/core/Auth/Template'
-
+const Login = () => {
+  return (
+  
+  )
+}
 
 export default Login
