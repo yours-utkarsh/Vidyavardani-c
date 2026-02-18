eb0468a6d2235d00b44e05154a9ab0bdb46fed51
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-19 00:03:49 +0530

    fix: adjust layout structure in ViewCourse component

--- src/pages/ViewCourse.jsx
+++ src/pages/ViewCourse.jsx
@@ -38,7 +38,11 @@ export default function ViewCourse() {
     <>
       <div className="relative flex min-h-[calc(100vh-3.5rem)]">
         <VideoDetailsSidebar setReviewModal={setReviewModal} />
-        
+        <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+          <div className="mx-6">
+            <Outlet />
+          </div>
+        </div>
       </div>
       
     </>
