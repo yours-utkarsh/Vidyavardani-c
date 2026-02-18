e6cd67c4a44b5eccacfc8756ecb6620079a4fc89
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-18 23:56:29 +0530

    add structure to Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,9 @@ import loginImg from '../asset/Image/login.webp'
 import Template from '../components/core/Auth/Template'
 const Login = () => {
   return (
-  
+    <div>
+     
+    </div>
   )
 }
 
