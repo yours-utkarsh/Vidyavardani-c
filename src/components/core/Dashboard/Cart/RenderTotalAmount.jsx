7d6887ffa1447e3fbae212a6efdb668c67d54bab
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-22 18:44:38 +0530

    implemented func

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -5,4 +5,11 @@ import { useNavigate } from "react-router-dom"
 import IconBtn from "../../../common/IconBtn"
 // import IconBtn from "../../../Common/IconBtn"
 
- 
+export default function RenderTotalAmount() {
+  const { total, cart } = useSelector((state) => state.cart)
+  const { token } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.profile)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  
