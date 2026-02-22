ceb545e6b6392bdb6ffdbf42cbc1dc98ba2cecd3
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-22 18:45:19 +0530

    outer box implemented

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -19,6 +19,9 @@ export default function RenderTotalAmount() {
   }
 
   return (
-   
+    <div className="min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
+      <p className="mb-1 text-sm font-medium text-richblack-300">Total:</p>
+     
+    </div>
   )
 }
