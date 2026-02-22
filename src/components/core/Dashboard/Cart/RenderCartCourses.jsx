61f5fa7a03bd548ec363907bd2020f4baa5c702e
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-22 18:41:20 +0530

    outer box build

--- src/components/core/Dashboard/Cart/RenderCartCourses.jsx
+++ src/components/core/Dashboard/Cart/RenderCartCourses.jsx
@@ -7,5 +7,9 @@ import { removeFromCart } from "../../../../slices/cartSlice"
 export default function RenderCartCourses() {
   const { cart } = useSelector((state) => state.cart)
   const dispatch = useDispatch()
- 
+  return (
+    <div className="flex flex-1 flex-col">
+     
+    </div>
+  )
 }
