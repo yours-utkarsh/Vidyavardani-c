8b0ba69f0335f97a74c1c1eca2683629d641cd30
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-23 08:18:16 +0530

    daat added

--- src/data/Footer-Link.js
+++ src/data/Footer-Link.js
@@ -4,7 +4,9 @@ export const FooterLink2 = [
   
     links: [
       
-      
+        {title: "Professional certification", link: "/professional-certification"},
+        {title: "Full Catalog", link: "/full-catalog"},
+        {title: "Beta Content", link: "/beta-content"}
     ]
   }
 ];
