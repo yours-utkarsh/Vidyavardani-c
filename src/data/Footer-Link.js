93c99b826275171c41f9a36b640dc023545e2f50
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-23 08:18:27 +0530

    titile 2

--- src/data/Footer-Link.js
+++ src/data/Footer-Link.js
@@ -3,7 +3,9 @@ export const FooterLink2 = [
   {
   
     links: [
-      
+        {title: "Career paths", link: "/career-paths"},
+        {title: "Career services", link: "/career-services"},
+        {title: "Interview prep", link: "/interview-prep"},
         {title: "Professional certification", link: "/professional-certification"},
         {title: "Full Catalog", link: "/full-catalog"},
         {title: "Beta Content", link: "/beta-content"}
