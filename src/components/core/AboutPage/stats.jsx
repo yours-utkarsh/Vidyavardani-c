77a42ade3b777413adb1fd67bc67efad15a3f913
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-26 23:38:04 +0530

    map on stats array applied

--- src/components/core/AboutPage/stats.jsx
+++ src/components/core/AboutPage/stats.jsx
@@ -13,7 +13,9 @@ const StatsComponenet = () => {
       {/* Stats */}
       <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto ">
         <div className="grid grid-cols-2 md:grid-cols-4 text-center">
-        
+          {Stats.map((data, index) => {
+          
+          })}
         </div>
       </div>
     </div>
