236d8e44b6e294d3684416f050e0786dea84c02f
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-26 23:37:51 +0530

    full structure established

--- src/components/core/AboutPage/stats.jsx
+++ src/components/core/AboutPage/stats.jsx
@@ -11,7 +11,11 @@ const StatsComponenet = () => {
   return (
     <div className="bg-richblack-700 mb-16">
       {/* Stats */}
-     
+      <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto ">
+        <div className="grid grid-cols-2 md:grid-cols-4 text-center">
+        
+        </div>
+      </div>
     </div>
   );
 };
