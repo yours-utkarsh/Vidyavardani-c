55c779733345a7e5582bc44cb01909f49694308c
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-26 23:38:38 +0530

    data count added

--- src/components/core/AboutPage/stats.jsx
+++ src/components/core/AboutPage/stats.jsx
@@ -16,7 +16,9 @@ const StatsComponenet = () => {
           {Stats.map((data, index) => {
             return (
               <div className="flex flex-col py-10" key={index}>
-               
+                <h1 className="text-[30px] font-bold text-richblack-5">
+                  {data.count}
+                </h1>
                 <h2 className="font-semibold text-[16px] text-richblack-500">
                   {data.label}
                 </h2>
