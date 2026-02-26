ffd5fedef2d1fbd8bf17cc929b0caa64d4601ae9
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-26 23:38:16 +0530

    return statement

--- src/components/core/AboutPage/stats.jsx
+++ src/components/core/AboutPage/stats.jsx
@@ -14,7 +14,12 @@ const StatsComponenet = () => {
       <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto ">
         <div className="grid grid-cols-2 md:grid-cols-4 text-center">
           {Stats.map((data, index) => {
-          
+            return (
+              <div className="flex flex-col py-10" key={index}>
+               
+             
+              </div>
+            );
           })}
         </div>
       </div>
