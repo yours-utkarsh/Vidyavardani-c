ec258f3a3ba209a17649e0c90e197143b6631e9b
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-26 23:37:08 +0530

    data added

--- src/components/core/AboutPage/stats.jsx
+++ src/components/core/AboutPage/stats.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+const Stats = [
+  { count: "5K", label: "Active Students" },
+  { count: "10+", label: "Mentors" },
+  { count: "200+", label: "Courses" },
+  { count: "50+", label: "Awards" },
+];
+
 const StatsComponenet = () => {
   return (
    
