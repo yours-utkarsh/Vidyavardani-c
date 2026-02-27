15f45b7ab6243b2110098f5c7d9fcd94dc3ddf19
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-27 17:42:12 +0530

    half count start and empty start calc

--- src/components/common/RatingStars.jsx
+++ src/components/common/RatingStars.jsx
@@ -23,7 +23,9 @@ function RatingStars({ Review_Count, Star_Size }) {
   return (
     <div className="flex gap-1 text-yellow-100">
     
-     
+      {[...new Array(starCount.half)].map((_, i) => {
+        return <TiStarHalfOutline key={i} size={Star_Size || 20} />
+      })}
       {[...new Array(starCount.empty)].map((_, i) => {
         return <TiStarOutline key={i} size={Star_Size || 20} />
       })}
