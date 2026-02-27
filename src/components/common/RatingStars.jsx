4b2c54f922abec7cfba7efe067792ca80d93b3c5
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-27 17:42:26 +0530

    full star count

--- src/components/common/RatingStars.jsx
+++ src/components/common/RatingStars.jsx
@@ -22,7 +22,9 @@ function RatingStars({ Review_Count, Star_Size }) {
   }, [Review_Count])
   return (
     <div className="flex gap-1 text-yellow-100">
-    
+      {[...new Array(starCount.full)].map((_, i) => {
+        return <TiStarFullOutline key={i} size={Star_Size || 20} />
+      })}
       {[...new Array(starCount.half)].map((_, i) => {
         return <TiStarHalfOutline key={i} size={Star_Size || 20} />
       })}
