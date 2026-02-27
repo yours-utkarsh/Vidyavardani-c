a545e7e30c775b94381aacc726d49936f9afc68c
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-27 18:23:18 +0530

    imports init

---
+++ src/components/common/ReviewSlider.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import ReactStars from "react-rating-stars-component";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "../../App.css";
+import "../../App.css";
+import { FaStar } from "react-icons/fa";
+import { Autoplay, FreeMode, Pagination } from "swiper/modules";
+import { apiConnector } from "../../services/apiconnector";
+import { ratingsEndpoints } from "../../services/apis";
+
+function ReviewSlider() {
+ 
+
+ 
+  return (
+   
+  );
+}
+
+export default ReviewSlider;
