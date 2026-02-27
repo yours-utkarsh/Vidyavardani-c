8c3d144671b0930eb24b72de65c25068db58611e
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-27 17:41:24 +0530

    use effect for satrt count

--- src/components/common/RatingStars.jsx
+++ src/components/common/RatingStars.jsx
@@ -12,7 +12,14 @@ function RatingStars({ Review_Count, Star_Size }) {
     empty: 0,
   })
 
- 
+  useEffect(() => {
+    const wholeStars = Math.floor(Review_Count) || 0
+    SetStarCount({
+      full: wholeStars,
+      half: Number.isInteger(Review_Count) ? 0 : 1,
+      empty: Number.isInteger(Review_Count) ? 5 - wholeStars : 4 - wholeStars,
+    })
+  }, [Review_Count])
  
 }
 
