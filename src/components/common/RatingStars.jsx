dcb7dba5c19617317d862f639f28e3759d4c6135
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-02-27 17:41:01 +0530

    imports

---
+++ src/components/common/RatingStars.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react"
+import {
+  TiStarFullOutline,
+  TiStarHalfOutline,
+  TiStarOutline,
+} from "react-icons/ti"
+
+
+ 
+ 
+}
+
+export default RatingStars
