0fd13c3b5a6b012ef631d2a85c343eb193662435
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-03-01 23:22:35 +0530

    listener function

--- src/Hooks/useOnClickOutside.jsx
+++ src/Hooks/useOnClickOutside.jsx
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 
 const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
-   
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+    
+    };
    
     
     return () => {
