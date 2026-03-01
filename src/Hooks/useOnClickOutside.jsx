5a7a4eda7b0d067dade82299ac195b5c45d9c318
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-03-01 23:22:06 +0530

    useeffect cycle

--- src/Hooks/useOnClickOutside.jsx
+++ src/Hooks/useOnClickOutside.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
 
 const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+   
+   
+    
   
+  }, []);
 };
 
 export default useOnClickOutside;
