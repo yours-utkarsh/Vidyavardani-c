04d42c3453693f332bd98632d1304a256d48d05e
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-03-02 23:38:43 +0530

    basic functionalities and return statements

--- src/Hooks/useRouteMatch.jsx
+++ src/Hooks/useRouteMatch.jsx
@@ -2,3 +2,8 @@ import React from 'react'
 import { matchPath, useLocation } from 'react-router-dom'
 
 const useRouteMatch = (path) => {
+    const location = useLocation();
+  return matchPath(location.pathname, { path })
+}
+
+export default useRouteMatch
