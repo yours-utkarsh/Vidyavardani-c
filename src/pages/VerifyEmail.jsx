17d599dac69ac9ff009a7be6fa7aa48bfd53e445
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-03-04 23:09:09 +0530

    verify email

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -47,7 +47,15 @@ const VerifyEmail = () => {
       {loading ? (
         <Spinner></Spinner>
       ) : (
+          <div className="max-w-[500px] p-4 lg:p-8">
+          <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">
+            Verify Email
+          </h1>
+       
+        
          
+
+        </div>
       )}
     </div>
   );
