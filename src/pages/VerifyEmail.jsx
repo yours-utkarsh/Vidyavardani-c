e200ae445e0f5e5dbc9027c58380eae6c9ca639c
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-03-04 23:10:17 +0530

    button implemented

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -72,7 +72,10 @@ const VerifyEmail = () => {
                 gap: "0 6px",
               }}
             />
-          
+            <button
+              type="submit"
+              className="w-full bg-yellow-50 py-[12px] px-[12px] rounded-[8px] mt-6 font-medium text-richblack-900"
+            >Verify Email</button>
           </form>
          
 
