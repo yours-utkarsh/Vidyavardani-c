375b9d938d2beae84168bc99abf813eabd524c4f
Utkarsh Gupta <utkarshg9981@gmail.com>
2026-03-04 23:07:12 +0530

    basic func implemented

---
+++ src/pages/VerifyEmail.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Spinner from "../components/common/Spinner";
+import OTPInput from "react-otp-input";
+import { sendOtp, signup } from "../services/operations/authAPI";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { RxCountdownTimer } from "react-icons/rx";
+import { BiArrowBack } from "react-icons/bi";
+
+const VerifyEmail = () => {
+  
+
+  
+
+   
+  }
+  return (
+     <div className="min-h-[calc(100vh-3.5rem)] grid place-items-center">
+     
+    </div>
+  );
+};
+
+export default VerifyEmail;
